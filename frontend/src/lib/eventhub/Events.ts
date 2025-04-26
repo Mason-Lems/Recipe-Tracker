3d9adc36b5ff9996359e5a1fbee95c3470b4cc7d
Mason Lem <mlemberger@umass.edu>
2025-04-25 20:44:04 -0400

    Added event list

---
+++ frontend/src/lib/eventhub/Events.ts
@@ -0,0 +1,12 @@
+/**
+ * An object containing all the events that can be emitted by the event hub.
+ * * @typedef {Object} Events
+ */
+
+export const Events = {
+    NewRecipe: "NewRecipe",
+    RecipeUpdated: "RecipeUpdated",
+    RecipeDeleted: "RecipeDeleted",
+
+    NavigateTo: "NavigateTo",
+}
