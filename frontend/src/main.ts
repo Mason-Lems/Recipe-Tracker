9bc751df1dc152b5177fb5f94dd6068281d168df
Mason Lem <mlemberger@umass.edu>
2025-04-29 15:54:41 -0400

    Finished main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,23 @@
 import App from "./app"
 import { Events, EventHub } from "./lib/eventhub/index";
-import "./style.css"
+
+const app = new App();
+const hub = EventHub.getInstance();
+
+// if use nagivates to other pages using the browersers back/forward buttons
+// or the url bar, this will trigger the nagivation event to render that page
+document.addEventListener('DOMContentLoaded', () => {
+    const url = new URL(window.location.href);
+    const path = url.pathname + url.search;
+    const rootContainer = document.getElementById("root");
+
+    if (rootContainer) rootContainer.appendChild(app.render());
+
+    hub.publish(Events.NavigateTo, path);
+});
+
+window.addEventListener("popstate", () => {
+    const url = new URL(window.location.href);
+    const path = url.pathname + url.search;
+    hub.publish(Events.NavigateTo, path);
+})
