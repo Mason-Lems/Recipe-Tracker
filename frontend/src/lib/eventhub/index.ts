296828a41611618149cba677bb823a6732fe945d
Mason Lem <mlemberger@umass.edu>
2025-04-29 15:55:58 -0400

    "ran npm install"

--- frontend/src/lib/eventhub/index.ts
+++ frontend/src/lib/eventhub/index.ts
@@ -1,7 +1,7 @@
 import { EventHub } from "@/lib/eventhub/EventHub";
 import { Events } from "@/lib/eventhub/Events";
 
-const hub = EventHub.getInstance(); //Get the event hub instance
+//const hub = EventHub.getInstance(); //Get the event hub instance
 
 
 export { EventHub, Events };
